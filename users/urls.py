f52ae9db9bc5d75c2740246a14ab8d55544af1ae
Abhi <abhishek802314@gmail.com>
2023-02-24 08:19:42 +0530

    first commit

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views 
+from django.contrib.auth import views as auth_view
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password_change/', auth_view.PasswordChangeView.as_view(), name='password_change'),
+
+]
